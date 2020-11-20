f536c5d29d65afa164cebc9f6e85974252efa2b2
DAngelo Thomas <dangelo1thomas@gmail.com>
2020-11-19 19:43:20 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class BankAccount
+
+
+    attr_accessor 
+    attr_reader :name, :status, :balance
+    attr_writer :balance, :status
+
+    def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+    end
+
+     def deposit(money)
+        #binding.pry
+        @balance += money
+     end
+
+     def display_balance
+        "Your balance is $#{balance}." 
+     end
+
+     def valid?
+        @status == "open" && @balance > 0
+     end
+
+     def close_account
+        self.status = "closed"
+     end
+
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,19 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :amount, :bank_account
+  attr_reader :status
+
+  def initialize(sender, receiver, name)
+    @name = name
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = 50
+  end
+
+  def valid?(name)
+     if status == "open" 
+      name.receiver
+     end
+  end
 end
